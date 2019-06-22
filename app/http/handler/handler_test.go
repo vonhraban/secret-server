aea1b36ba0bb4800bec2c525dade702d839b378f
Vlad <hraban@vivec.co.uk>
2019-06-22 19:44:43 +0200

    Pass the logger to tests

--- app/http/handler/handler_test.go
+++ app/http/handler/handler_test.go
@@ -8,12 +8,15 @@ import (
 	"strings"
 	"time"
 
+	"github.com/sirupsen/logrus"
 	"github.com/gorilla/mux"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/vonhraban/secret-server/app/http/handler"
 	"github.com/vonhraban/secret-server/persistence"
 	"github.com/vonhraban/secret-server/secret"
+	"github.com/vonhraban/secret-server/core/log"
+
 )
 
 // Set up time travel
@@ -32,7 +35,7 @@ func (d *deterministicClock) GetCurrentTime() time.Time {
 var _ = Describe("Secret Handler", func() {
 	clock := &deterministicClock{}
 	vault := persistence.NewInMemoryVault(clock)
-	secretHandler := handler.NewSecretHandler(vault, clock)
+	secretHandler := handler.NewSecretHandler(vault, clock, log.NewLogrusLogger(logrus.New()))
 
 	Context("Given it is 2019-06-15 11:14:23", func() {
 		timeValue, err := time.Parse("2006-01-02 15:04:05", "2019-06-15 11:14:23")
